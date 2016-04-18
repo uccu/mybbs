6a3695429b1cb4b50d05d84b901053c71fd7c5be
uccu <418667631@qq.com>
2016-04-19 00:25:53 +0800

    up

--- source/plugin/seanime/template/index.php
+++ source/plugin/seanime/template/index.php
@@ -10,15 +10,14 @@
     
     <div class="box-b1">
         <div class="box-p1 w-200">
-            <a href="seanime/index#box-1" class="t box-p1 w-80 h-80 bgc-2 bgc-h2"><i>资源</i></a>
-            <a href="seanime/index#box-2" class="t box-p1 w-80 h-80 bgc-4 bgc-h4"><i>节目单</i></a>
+            <a href="seanime/index#box-1" class="t box-p1 w-80 h-80 bgc-1 bgc-h1"><i>资源</i></a>
+            <a href="seanime/index#box-2" class="t box-p1 w-80 h-80 bgc-2 bgc-h2"><i>节目单</i></a>
             <a href="seanime/index#box-3" class="t box-p1 w-80 h-80 bgc-3 bgc-h3"><i>游戏</i></a>
-            <a href="seanime/index#box-4" class="t box-p1 w-80 h-80 bgc-1 bgc-h1"><i>签到</i></a>
-            <a href="seanime/index#box-5" class="t box-p1 w-80 h-80 bgc-1 bgc-h1"><i>讨论</i></a>
-            <a href="seanime/index#box-5" class="t box-p1 w-80 h-80 bgc-1 bgc-h1"><i>发布</i></a>
-            <div class="t box-p1 w-80 h-80 bgc-3 bgc-h3"><i>ceshi</i></div>
-            
-            <div class="t box-p1 w-80 h-80 bgc-7 bgc-h7"><i>ceshi</i></div>
+            <a href="seanime/index#box-4" class="t box-p1 w-80 h-80 bgc-5 bgc-h5"><i>签到</i></a>
+            <a href="seanime/index#box-5" class="t box-p1 w-80 h-80 bgc-6 bgc-h6"><i>讨论</i></a>
+            <a href="seanime/index#box-6" class="t box-p1 w-80 h-80 bgc-7 bgc-h7"><i>发布</i></a>
+            <a href="seanime/index#overlay-1" class="t box-p1 w-80 h-80 bgc-4 bgc-h4"><i>ceshi</i></a>
+            <a href="seanime/index#overlay-1" class="t box-p1 w-80 h-80 bgc-4 bgc-h4"><i>ceshi</i></a>
         </div>
         
         <div id="box-1" class="box-p1 w-400 tran-2 dn">
@@ -27,10 +26,13 @@
             <div class="t box-p1 w-380 h-180 bgc-1 bgc-h1"><i>All</i></div>
         </div>
         <div id="box-2" class="box-p1 w-400 tran-2 dn">
-            <div class="t box-p1 w-180 h-180 bgc-3 bgc-h3"><i>ceshi</i></div>
-            <div class="t box-p1 w-180 h-180 bgc-3 bgc-h3"><i>ceshi</i></div>
-            <div class="t box-p1 w-180 h-180 bgc-3 bgc-h3"><i>ceshi</i></div>
-            <div class="t box-p1 w-180 h-180 bgc-3 bgc-h3"><i>ceshi</i></div>
+            <div class="t box-p1 w-180 h-180 bgc-4 bgc-h4"><i>ceshi</i></div>
+            <div class="t box-p1 w-180 h-180 bgc-4 bgc-h4"><i>ceshi</i></div>
+            <div class="t box-p1 w-180 h-180 bgc-4 bgc-h4"><i>ceshi</i></div>
+            <div class="t box-p1 w-180 h-180">
+                <div class="t box-p1 w-180 h-180 bgc-4 bgc-h4 pa left-0 top-0 margin-auto"><i>ceshi</i></div>
+                <div class="t box-p1 w-180 h-180 bgc-4 bgc-h4 pa left-0 top-0 margin-auto"><i>ceshi</i></div>
+            </div>
         </div>
         <div id="box-3" class="box-p1 w-400 tran-2 dn">
             <div class="t box-p1 w-180 h-180 bgc-4 bgc-h4"><i>ceshi</i></div>
@@ -38,13 +40,36 @@
             <div class="t box-p1 w-180 h-180 bgc-4 bgc-h4"><i>ceshi</i></div>
             <div class="t box-p1 w-180 h-180 bgc-4 bgc-h4"><i>ceshi</i></div>
         </div>
+        <div id="box-4" class="box-p1 w-400 tran-2 dn">
+            <div class="t box-p1 w-180 h-180 bgc-4 bgc-h4"><i>ceshi</i></div>
+            <div class="t box-p1 w-180 h-180 bgc-4 bgc-h4"><i>ceshi</i></div>
+            <div class="t box-p1 w-180 h-180 bgc-4 bgc-h4"><i>ceshi</i></div>
+            <div class="t box-p1 w-180 h-180 bgc-4 bgc-h4"><i>ceshi</i></div>
+        </div>
+        <div id="box-5" class="box-p1 w-400 tran-2 dn">
+            <div class="t box-p1 w-180 h-180 bgc-4 bgc-h4"><i>ceshi</i></div>
+            <div class="t box-p1 w-180 h-180 bgc-4 bgc-h4"><i>ceshi</i></div>
+            <div class="t box-p1 w-180 h-180 bgc-4 bgc-h4"><i>ceshi</i></div>
+            <div class="t box-p1 w-180 h-180 bgc-4 bgc-h4"><i>ceshi</i></div>
+        </div>
+        <div id="box-6" class="box-p1 w-400 tran-2 dn">
+            <div class="t box-p1 w-180 h-180 bgc-4 bgc-h4"><i>ceshi</i></div>
+            <div class="t box-p1 w-180 h-180 bgc-4 bgc-h4"><i>ceshi</i></div>
+            <div class="t box-p1 w-180 h-180 bgc-4 bgc-h4"><i>ceshi</i></div>
+            <div class="t box-p1 w-180 h-180 bgc-4 bgc-h4"><i>ceshi</i></div>
+        </div>
         <div id="" class="box-p1 w-400 tran-2">
-            <div class="t box-p1 w-380 h-380 bgc-1 bgc-h1"><i>ceshi</i></div>
+            <div class="t box-p1 w-380 h-380 bgc-4 bgc-h4"><i>ceshi</i></div>
         </div>
     </div>
 </div>
 
-
+<div id="overlay-1" class="overlay overlay_cancel nos cd">
+    <div class="overlay_bg"></div>
+    <div class="overlay_box">
+        <div class="box-o2"><i>未开通</i></div>
+    </div>
+</div>
 
 
 
