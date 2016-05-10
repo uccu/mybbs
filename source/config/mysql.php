550169935dfc66cd24a38db6ca86d07ae37744af
uccu <mbmbn123@hotmail.com>
2016-05-10 17:07:25 +0800

    Update mysql.php

--- source/config/mysql.php
+++ source/config/mysql.php
@@ -2,27 +2,13 @@
 if(!defined('IN_PLAY')) {
 	exit('Access Denied');
 }
-// $config=array(
-	// 'host'=>'56e624c81f46e.sh.cdb.myqcloud.com:5884',
-	// 'user'=>'cdb_outerroot',
-	// 'password'=>'qqwwee22zxp',
-	// 'db'=>'sources'
-// );
 
-$config=array(
-	'host'=>'56e624c81f46e.sh.cdb.myqcloud.com:5884',
-	'user'=>'cdb_outerroot',
-	'password'=>'qqwwee22zxp',
-	'db'=>'sources',
-	'prefix'=>'',
-	'charset'=>'utf8'
-);
 
 // $config=array(
 	// 'host'=>'uugggggggggggggg.yoooo.co',
-	// 'user'=>'baka3',
-	// 'password'=>'123',
-	// 'db'=>'sources',
+	// 'user'=>'',
+	// 'password'=>'',
+	// 'db'=>'so',
 	// 'prefix'=>'',
 	// 'charset'=>'utf8'
 // );
