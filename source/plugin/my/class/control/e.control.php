f342c459cb0ecd85ee35ef2cefc07d4db3cd3ccf
uccu <mbmbn123@hotmail.com>
2016-05-11 17:19:24 +0800

    up

---
+++ source/plugin/my/class/control/e.control.php
@@ -0,0 +1,16 @@
+<?php
+namespace plugin\my\control;
+defined('IN_PLAY') || exit('Access Denied');
+use \control;
+class e extends control{
+    function _beginning(){
+        T();
+        
+    }
+    
+    
+    
+}
+
+
+?>
---
+++ source/plugin/my/template/e.php
@@ -0,0 +1,31 @@
+<!--{subtemplate tool:header}-->
+<div class="jumbotron">
+    <div class="container">
+        <h1>Hello, world!</h1>
+        <p>...</p>
+        <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+    </div>
+</div>
+<div class="container">
+    <div class="page-header">
+        <h1>Example page header <small>Subtext for header</small></h1>
+    </div>
+</div>
+<div class="container">
+    <div class="panel panel-default">
+        <div class="panel-body">
+            <p>{g.template.cacheid}</p>
+            <p>For example, <code>&lt;section&gt;</code> should be wrapped as inline.</p>
+            <h1>h1. Bootstrap heading</h1>
+            <p>Bootstrap <b>heading</b></p>
+        </div>
+    </div>
+</div>
+<div class="container">
+<ul class="nav nav-tabs">
+    <li role="presentation"><a href="#">Home</a></li>
+    <li role="presentation" class="active"><a href="#">Profile</a></li>
+    <li role="presentation"><a href="#">Messages</a></li>
+</ul>
+</div>
+<!--{subtemplate tool:footer}-->
