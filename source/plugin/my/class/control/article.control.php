c581ccc9e84fc6e2f4e829d57e09fe994f28c9fa
uccu <418667631@qq.com>
2016-05-13 23:38:29 +0800

    up

--- source/plugin/my/class/control/article.control.php
+++ source/plugin/my/class/control/article.control.php
@@ -9,8 +9,8 @@ class article extends \control\ajax{
         return model('my:article');
     }
     private function unseri(&$n){
-        $n = preg_replace('/\{img:([^\}]+):\}/i','<img src="$1" class="img-responsive" />',$n);
-        
+        $n = preg_replace('/\{img:([0-9]{1,2})\/([a-z0-9]{16})\.(jpg|png|gif):\}/i','<img src="s$1/$2.$3" class="img-responsive" />',$n);
+         
         "{b:string:}";'<strong>string</strong>';
         "{color=#fff:}";
         "{size=20px:}";
@@ -22,6 +22,7 @@ class article extends \control\ajax{
         if(!$f = $m->find($aid))$this->error('401','no article');
         $f['date'] = date('Y-m-d H:i:s',$f['ctime']);
         $f['passage'] = explode('\n',$f['description']);
+        
         foreach($f['passage'] as &$n){
             $this->unseri($n);
         }
