31a5309ad6a0db17ec431f0ec2431d00bb77a650
uccu <418667631@qq.com>
2016-06-20 22:12:32 +0800

    h

--- source/plugin/adminloader/template/_header.php
+++ source/plugin/adminloader/template/_header.php
@@ -1,8 +1,8 @@
 <!--{subtemplate tool:header}-->
 <!--{eval addjs('p',0,'adminloader')}-->
 <!--{eval addjs('inform',0,'adminloader')}-->
-<script type="text/javascript" src='ueditor/ueditor.config.js'></script>
-<script type="text/javascript" src='ueditor/ueditor.all.min.js'></script>
+<!--<script type="text/javascript" src='ueditor/ueditor.config.js'></script>
+<script type="text/javascript" src='ueditor/ueditor.all.min.js'></script>-->
 <style>
     .modal-backdrop{z-index:1}
     .modal{z-index:2}
@@ -17,12 +17,12 @@
         <style>
             .delSuccess:target,.saveSuccess:target{display:block}
         </style>
-        <div id="alert" class="delSuccess alert alert-danger alert-dismissible fade in dn" role="alert">
+        <div id="delSuccess" class="delSuccess alert alert-danger alert-dismissible fade in dn" role="alert">
             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">×</span></button>
             <p>删除成功</p>
         </div>
-        <div id="alert" class="saveSuccess alert alert-danger alert-dismissible fade in dn" role="alert">
+        <div id="saveSuccess" class="saveSuccess alert alert-danger alert-dismissible fade in dn" role="alert">
             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">×</span></button>
             <p>保存成功</p>
