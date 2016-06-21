bcc22a0415157ed6dc1afc7e4dcb7c1c63eaa50c
uccu <DuanYaFei@jyliMac.local>
2016-06-21 10:17:52 +0800

    da

--- source/plugin/testadmin/class/control/admin.control.php
+++ source/plugin/testadmin/class/control/admin.control.php
@@ -3,9 +3,8 @@ namespace plugin\testadmin\control;
 defined('IN_PLAY') || exit('Access Denied');
 class admin{
     function up_pic(){
-        
-
-
+        $a = control('upload','picture')->_get_srcs();
+        var_dump($a);
     }
     
 
