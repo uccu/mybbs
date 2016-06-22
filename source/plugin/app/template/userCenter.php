b3ef8186b5a30204a72414c144b59971a81198b9
uccu <DuanYaFei@jyliMac.local>
2016-06-22 17:47:58 +0800

    sqs

--- source/plugin/app/template/userCenter.php
+++ source/plugin/app/template/userCenter.php
@@ -6,3 +6,39 @@
 <!--{/loop}-->
 </pre>
 
+
+<base href="{baseurl}">
+
+{baseurl}
+
+{title}
+{keywords}
+{description}
+
+
+{$coser.uid}
+
+{$coser['uid']}
+
+//1
+
+<!--{loop $album $k=>$p}-->   //foreach($album as $k=>$v){
+    {p.aid}
+
+
+
+
+<!--{/loop}-->                 //  }
+
+<!--{if $me == 'adasd'}-->
+
+
+<!--{elseif $me == 'adasd'}-->
+<!--{else}-->
+<!--{/if}-->
+
+
+
+<!--{subtemplate album/admin}-->
+
+
