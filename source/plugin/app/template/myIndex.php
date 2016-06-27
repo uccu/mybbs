d88c393f4ae47d484d61c9ec43a285caaae44411
uccu <DuanYaFei@jyliMac.local>
2016-06-27 11:28:32 +0800

    sq

--- source/plugin/app/template/myIndex.php
+++ source/plugin/app/template/myIndex.php
@@ -1,9 +1,10 @@
 <!--{subtemplate _header}-->
 <style>
 .banner{height:150px}
-.banner_pic,.banner_pic_block{width:100%;height:100%;background-color:#ccc;}
-.banner_pic_block{background-size:cover}
+.banner_pic,.banner_pic_block{width:100%;height:100%;background-color:#ccc;top:0;bottom:0}
+.banner_pic_block{background-size:cover;}
 .banner>span{position: absolute;right: 6px;bottom: 6px;z-index:3}
+.banner>span li{cursor:pointer}
 .main-box{margin-top:25px}
 
 </style>
@@ -12,9 +13,9 @@
         <div class="col-sm-9">
             <div class="banner pr">
                 <div class="banner_pic pa">
-                    <div class="banner_pic_block" style="background-iamge:url()"></div>
-                    <div class="banner_pic_block"></div>
-                    <div class="banner_pic_block"></div>
+                    <div class="banner_pic_block pa" style="background-iamge:url()"></div>
+                    <div class="banner_pic_block pa"></div>
+                    <div class="banner_pic_block pa"></div>
                 </div>
                 <span>
                     <ul class="list-inline">
@@ -26,7 +27,7 @@
             </div>
         </div>
         <div class="col-sm-3 myinfo hidden-xs">
-            <div class="myinfo">
+            <div class="myinfo"></div>
         
         </div>
        
