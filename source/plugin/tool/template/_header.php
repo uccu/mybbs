262ee4f33e7400971cc1ebe4bdcbef7bb849bf3d
uccu <DuanYaFei@jyliMac.local>
2016-08-24 16:04:38 +0800

    っd

---
+++ source/plugin/tool/template/_header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="keywords" content="{g.template.keywords}">
+<meta name="description" content="{g.template.description}">
+<title>{g.template.title}</title>
+<base href="{g.template.baseurl}">
+<!--{eval addcss('bootstrap.min',0,'tool')}-->
+
+<!--{eval addcss('component',0,'tool')}-->
+<!--{eval addcss('m',0,'tool')}-->
+<!--{eval addjs('jquery.min',0,'tool')}-->
+<!--{eval addjs('jquery.lazyload.min',0,'tool')}-->
+<!--{eval addjs('bootstrap.min',0,'tool')}-->
+<!--{eval addjs('md5',0,'tool')}-->
+<!--{eval addjs('enc-base64-min',0,'tool')}-->
+<!--[if lt IE 9]>
+      <script src="//apps.bdimg.com/libs/html5shiv/r29/html5.min.js"></script>
+      <script src="//apps.bdimg.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+<!--{eval addjs('p',0,'tool')}-->
+</head>
+<body>
