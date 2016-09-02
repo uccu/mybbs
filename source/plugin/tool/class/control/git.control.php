022b24f76f4e26579ca20eda2b92fbaf6a99ab6a
uccu <DuanYaFei@jyliMac.local>
2016-09-02 10:15:58 +0800

    e

---
+++ source/plugin/tool/class/control/git.control.php
@@ -0,0 +1,14 @@
+<?php
+namespace plugin\tool\control;
+defined('IN_PLAY') || exit('Access Denied');
+class git extends \control\ajax{
+    function _beginning(){
+        
+    }
+    function pull(){
+        system('d:\wamp\www\git_pull.bat',$out);
+        $this->success($out);
+    }
+
+}
+?>
