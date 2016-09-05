71c9857ca620c44e4b84904f98367510ed6e4af7
uccu <DuanYaFei@jyliMac.local>
2016-09-05 14:40:01 +0800

    w

--- source/plugin/app/class/control/home.control.php
+++ source/plugin/app/class/control/home.control.php
@@ -11,13 +11,23 @@ class home extends base\basic{
         $z = model('banner')->limit(999)->select();
         $q['bannerList'] = $z;
         if($this->out)$this->success($q);
+        return $q;
     }
     function nav(){
         $z = model('nav')->limit(999)->select();
         $q['navList'] = $z;
         if($this->out)$this->success($q);
+        return $q;
     }
-
+    function home(){
+        $this->out = false;
+        $q = array();
+        $q = array_merge($q,$this->banner());
+        $q = array_merge($q,$this->nav());
+        $q = array_merge($q,$this->activity());
+        $q = array_merge($q,$this->recommand());
+        $this->success($q);
+    }
     function activity(){
         $now = TIME_NOW;
         $where['zz'] = 'stime<$now AND etime>$now';
@@ -39,12 +49,22 @@ class home extends base\basic{
         }
         $q['previewActivityList'] = $z3;
         if($this->out)$this->success($q);
+        return $q;
     }
     function recommand(){
         $where['recommand'] = 1;
-        $z = model('activity')->where($where)->limit(999)->select();
+        $z = model('activity')->where($where)->limit(4)->select();
         $q['recommandList'] = array();
         if($this->out)$this->success($q);
+        return $q;
+    }
+    function recommand_c(){
+        $where['recommand'] = 1;
+        $page = post('page',2);
+        $z = model('activity')->where($where)->page($page,4)->select();
+        $q['recommandList'] = array();
+        if($this->out)$this->success($q);
+        return $q;
     }
 
 }
