aa422db470496a7f26c159b7279f933fe5ee112f
uccu <DuanYaFei@jyliMac.local>
2016-09-08 18:15:35 +0800

    dw

--- source/plugin/app/class/control/item.control.php
+++ source/plugin/app/class/control/item.control.php
@@ -213,27 +213,27 @@ class item extends base\basic{
             $data['name'] = $t['name'];
             model('cart')->remove($cid);
             if($this->out){
-                $zz2 = model('user_address')->where(array('uid'=>$this->uid,'type'=>1))->find();
-                $q['user']['name'] = $zz2['name']?$zz2['name']:'';
-                $q['user']['phone'] = $zz2['phone']?$zz2['phone']:'';
-                $q['user']['addr'] = $zz2['addr']?$zz2['addr']:'';
-                $l = model('location')->limit(9999)->select('id');
-                $v = &$zz2;
-                $v['area'] = $v['lid'];
-                $v['areaName'] = $l[$v['lid']]['title'];
-                if(!$v['area']){
-                    $v['area'] = 0;$v['areaName'] = '';
+                $q['user'] = model('user_address')->where(array('uid'=>$this->uid,'type'=>1))->find();
+                if($q['user']){
+                    $l = model('location')->limit(9999)->select('id');
+                    $v = &$q['user'];
+                    $v['area'] = $v['lid'];
+                    $v['areaName'] = $l[$v['lid']]['title'];
+                    if(!$v['area']){
+                        $v['area'] = 0;$v['areaName'] = '';
+                    }
+                    $v['city'] = $l[$v['lid']]['pid'];
+                    $v['cityName'] = $l[$v['city']]['title'];
+                    if(!$v['city']){
+                        $v['city'] = 0;$v['cityName'] = '';
+                    }
+                    $v['province'] = $l[$v['city']]['pid'];
+                    $v['provinceName'] = $l[$v['province']]['title'];
+                    if(!$v['province']){
+                        $v['province'] = 0;$v['provinceName'] = '';
+                    }
                 }
-                $v['city'] = $l[$v['lid']]['pid'];
-                $v['cityName'] = $l[$v['city']]['title'];
-                if(!$v['city']){
-                    $v['city'] = 0;$v['cityName'] = '';
-                }
-                $v['province'] = $l[$v['city']]['pid'];
-                $v['provinceName'] = $l[$v['province']]['title'];
-                if(!$v['province']){
-                    $v['province'] = 0;$v['provinceName'] = '';
-                }
+                
         
             }
         }
