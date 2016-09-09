e892148becf0eeb5b30789b85457aff6f21e0d74
uccu <DuanYaFei@jyliMac.local>
2016-09-09 16:42:33 +0800

    sd

--- source/plugin/app/class/control/rank.control.php
+++ source/plugin/app/class/control/rank.control.php
@@ -8,12 +8,13 @@ class rank extends base\basic{
 
 
     }
+
     function _allBean($aid){
         $all = model('order')->field('SUM(`bean`) as `s`')->where(array(
             'aid'=>$aid,
             'status'=>array('contain',array(2,3,4),'IN'),
         ))->find();
-        return $all['s']?$all['s']:0;
+        return $all['s']?$all['s']*0.5:0;
     }
     function get_c($rank,$c,$b){
  
@@ -52,7 +53,7 @@ class rank extends base\basic{
     function rank_gou($aid){
         //获取AID
         $aid = post('aid',$aid,'%d');
-        
+        $page = post('page',1);
         //获取活动所有乐豆
         $allBean = $z['allBean'] = $this->_allBean($aid);
 
@@ -75,14 +76,14 @@ class rank extends base\basic{
             'rank_bean'=>array(
                 '_on'=>'o.uid=b.uid AND o.aid=b.aid','_mapping'=>'b','bean','_join'=>'LEFT JOIN'
             )
-        ))->where($where)->order(array('pay_time'))->page(1,10)->select();
-        $this->_addCoin($z['list'],$allCoin,$rule);
+        ))->where($where)->order(array('pay_time'))->page($page,10)->select();
+        $this->_addCoin($z['list'],$allCoin,$rule,$page);
         $this->success($z);
     }
     function rank_xiang($aid){
         //获取AID
         $aid = post('aid',$aid,'%d');
-        
+        $page = post('page',1);
         //获取活动所有乐豆
         $allBean = $z['allBean'] = $this->_allBean($aid);
 
@@ -92,7 +93,7 @@ class rank extends base\basic{
 
         //获取排名
         $where['aid'] = $aid;
-        $where['first'] = 1;
+        $where['share_first'] = 1;
         $where['status'] = array('contain',array(2,3,4),'IN');
         $where['score'] = 0;
         $where['referee'] = array('logic',0,'!=');
@@ -106,14 +107,14 @@ class rank extends base\basic{
             'rank_bean'=>array(
                 '_on'=>'o.referee=b.uid AND o.aid=b.aid','_mapping'=>'b','bean','_join'=>'LEFT JOIN'
             )
-        ))->where($where)->order(array('pay_time'))->page(1,10)->select();
-        $this->_addCoin($z['list'],$allCoin,$rule);
+        ))->where($where)->order(array('pay_time'))->page($page,10)->select();
+        $this->_addCoin($z['list'],$allCoin,$rule,$page);
         $this->success($z);
     }
     function rank_bang($aid){
         //获取AID
         $aid = post('aid',$aid,'%d');
-        
+        $page = post('page',1);
         //获取活动所有乐豆
         $allBean = $z['allBean'] = $this->_allBean($aid);
 
@@ -122,26 +123,108 @@ class rank extends base\basic{
         $allCoin = $z['allCoin'] = $allBean*$rule['value']/100;
 
         $where['aid'] = post('aid',$aid);
-        $z['list'] = model('rank_bang')->where($where)->order(array('time'))->page(1,10)->select();
-        $this->_addCoin($z['list'],$allCoin,$rule);
+        $z['list'] = model('rank_bang')->where($where)->order(array('time'))->page($page,10)->select();
+        $this->_addCoin($z['list'],$allCoin,$rule,$page);
         $this->success($z);
     }
 
     function rank_dou($aid){
         //获取AID
         $aid = post('aid',$aid,'%d');
-        
+        $page = post('page',1);
         //获取活动所有乐豆
         $allBean = $z['allBean'] = $this->_allBean($aid);
 
         //获取当前排行的奖金
         $rule = model('rule')->find(4);
         $allCoin = $z['allCoin'] = $allBean*$rule['value']/100;
-        
+
         $where['aid'] = post('aid',$aid);
-        $z['list'] = model('rank_bean')->where($where)->order(array('bean'=>'DESC'))->page(1,10)->select();
-        $this->_addCoin($z['list'],$allCoin,$rule);
+        $z['list'] = model('rank_bean')->where($where)->order(array('bean'=>'DESC'))->page($page,10)->select();
+        $this->_addCoin($z['list'],$allCoin,$rule,$page);
         $this->success($z);
+    }
+    function my_rank($aid){
+
+        $this->_check_login();
+
+
+        $aid = post('aid',$aid,'%d');
+        $allBean = $z['allBean'] = $this->_allBean($aid);
+
+
+        $where = array();
+        $where['aid'] = $aid;
+        $where['uid'] = $this->uid;
+        $where['first'] = 1;
+        $where['status'] = array('contain',array(2,3,4),'IN');
+        $where['score'] = 0;
+        $me = model('order')->where($where)->order(array('pay_time'))->find();
+        if($me){
+            unset($where['uid']);
+            $where['pay_time'] = array('logic',$me['pay_time'],'<');
+            $z['gou']['rank'] = $rank = model('order')->where($where)->get_field()+1;
+            $rule = model('rule')->find(1);
+            $allCoin =  $allBean*$rule['value']/100;
+            $b = array($rule['value1']/100,$rule['value2']/100,$rule['value3']/100,$rule['value4']/100,$rule['value5']/100);
+            $z['gou']['coin'] = $coin = $this->get_c($rank,$allCoin,$b);
+        }else{
+            $z['gou']['rank'] = $z['gou']['coin'] = 0;
+        }
+
+
+        $where = array();
+        $where['aid'] = $aid;
+        $where['referer'] = $this->uid;
+        $where['share_first'] = 1;
+        $where['status'] = array('contain',array(2,3,4),'IN');
+        $where['score'] = 0;
+        $me = model('order')->where($where)->order(array('pay_time'))->find();
+        if($me){
+            unset($where['referer']);
+            $where['pay_time'] = array('logic',$me['pay_time'],'<');
+            $z['xiang']['rank'] = $rank = model('order')->where($where)->get_field()+1;
+            $rule = model('rule')->find(2);
+            $allCoin =  $allBean*$rule['value']/100;
+            $b = array($rule['value1']/100,$rule['value2']/100,$rule['value3']/100,$rule['value4']/100,$rule['value5']/100);
+            $z['xiang']['coin'] = $coin = $this->get_c($rank,$allCoin,$b);
+        }else{
+            $z['xiang']['rank'] = $z['xiang']['coin'] = 0;
+        }
+
+        $where = array();
+        $where['aid'] = $aid;
+        $where['uid'] = $this->uid;
+        $me = model('rank_bang')->where($where)->find();
+        if($me){
+            unset($where['uid']);
+            $where['time'] = array('logic',$me['time'],'<');
+            $z['bang']['rank'] = $rank = model('rank_bang')->where($where)->get_field()+1;
+            $rule = model('rule')->find(3);
+            $allCoin =  $allBean*$rule['value']/100;
+            $b = array($rule['value1']/100,$rule['value2']/100,$rule['value3']/100,$rule['value4']/100,$rule['value5']/100);
+            $z['bang']['coin'] = $coin = $this->get_c($rank,$allCoin,$b);
+        }else{
+            $z['bang']['rank'] = $z['bang']['coin'] = 0;
+        }
+
+        $where = array();
+        $where['aid'] = $aid;
+        $where['uid'] = $this->uid;
+        $me = model('rank_bean')->where($where)->find();
+        if($me){
+            unset($where['uid']);
+            $where['bean'] = array('logic',$me['bean'],'<');
+            $z['bean']['rank'] = $rank = model('rank_bean')->where($where)->get_field()+1;
+            $rule = model('rule')->find(4);
+            $allCoin =  $allBean*$rule['value']/100;
+            $b = array($rule['value1']/100,$rule['value2']/100,$rule['value3']/100,$rule['value4']/100,$rule['value5']/100);
+            $z['bean']['coin'] = $coin = $this->get_c($rank,$allCoin,$b);
+        }else{
+            $z['bean']['rank'] = $z['bean']['coin'] = 0;
+        }
+        $this->success($z);
+
     }
     function my_rank_gou(){
 
