d23e9b6741af4e7392c19eb986da27359bd18221
uccu <DuanYaFei@jyliMac.local>
2016-09-18 16:15:07 +0800

    dq

--- source/plugin/tool/class/control/captcha.control.php
+++ source/plugin/tool/class/control/captcha.control.php
@@ -6,11 +6,15 @@ class captcha extends \control\ajax{
         $this->checkAJAX = 0;
         //var_dump($this->uid);die();
     }
-    function get_captcha(){
-        $out = array(
-            'captcha'=>'1234'
-        );
-        $this->success($out);
+    function get_captcha($phone){
+        $phone = post('phone',$phone);
+        if(!preg_match('#^1\d{10}$#',$hpone))$this->error(500,'手机格式错误');
+        $s = '';$a = '1234567890';
+        for($i=0;$i<6;$i++){
+            $s.=$a[rand(0,9)];
+        }
+        $z = file_get_contents ("https://sapi.253.com/msg/HttpBatchSendSM?account=VIP-lssm-1&pswd=Key-147852&mobile={$phone}&needstatus=false&msg=您好，您的验证码是：{$s}");
+        $this->success();
     }
     function _check_captcha(){
 
