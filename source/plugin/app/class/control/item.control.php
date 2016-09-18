9f5943f4c38984bb4b2502c66682e3a0a18a485b
uccu <DuanYaFei@jyliMac.local>
2016-09-18 10:54:43 +0800

    123

--- source/plugin/app/class/control/item.control.php
+++ source/plugin/app/class/control/item.control.php
@@ -126,7 +126,7 @@ class item extends base\basic{
                     '_on'=>'tuan_cart.uid=c.uid AND tuan_cart.tid=c.tid','id'=>'collected','_mapping'=>'c','_join'=>'LEFT JOIN'
                 ),
                 'goods'=>array(
-                    'name','thumb','bean','price_act','price','_on'=>'tuan_cart.tid=g.tid','_mapping'=>'g'
+                    'name','thumb','bean','price_act','var','price','_on'=>'tuan_cart.tid=g.tid','_mapping'=>'g'
                 ),
                 'goods_attribute'=>array(
                     'attribute_name','_on'=>'lid'
