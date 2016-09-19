f89faac7fba69af9145996b4d972ac9a5cd19c9c
uccu <DuanYaFei@jyliMac.local>
2016-09-19 14:37:25 +0800

    123

--- source/plugin/app/class/control/rank.control.php
+++ source/plugin/app/class/control/rank.control.php
@@ -422,9 +422,9 @@ class rank extends base\basic{
         $where['score'] = 0;
         $uid = $this->uid;
         $q['list'] = model('logic')->fetch_all(
-            "SELECT o1.*,u.username,u.avatar,o2.referee as re2 FROM `order` o1 
-            left join `order` o2 on o1.referee = o2.uid 
-            inner join user u on o1.uid = u.uid 
+            "SELECT o1.*,u.username,u.avatar,o2.referee as re2 FROM `tuan_order` o1 
+            left join `tuan_order` o2 on o1.referee = o2.uid 
+            inner join tuan_user u on o1.uid = u.uid 
             WHERE ( o1.referee = $uid OR o2.referee = $uid ) AND o1.aid = $aid AND o1.status IN (2,3,4) AND o1.score = 0 ORDER BY o1.pay_time");
         if(!$q['list'])$this->errorCode(427);
         foreach($q['list'] as &$v){
