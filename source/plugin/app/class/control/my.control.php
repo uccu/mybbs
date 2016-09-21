5be7b44ea0efdc5704009a0cab21f2b27dbbe6c7
uccu <DuanYaFei@jyliMac.local>
2016-09-21 11:22:47 +0800

    de

--- source/plugin/app/class/control/my.control.php
+++ source/plugin/app/class/control/my.control.php
@@ -404,7 +404,7 @@ class my extends base\basic{
     function add_fans($uid){
         $uid = post('uid',$uid,'%d');
         $data['fans_id'] = $this->uid;
-        if(model('user')->find($uid))$this->errorCode(440);
+        if(!model('user')->find($uid))$this->errorCode(440);
         $data['uid'] = $uid;
         $data['aid'] = $this->aid;
         if(!model('fans')->where($data)->find()){
