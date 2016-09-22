54397e0da2ac83710844224d8275efa718ba31ba
uccu <DuanYaFei@jyliMac.local>
2016-09-22 17:44:24 +0800

    dw

--- pay_c/wcpay.php
+++ pay_c/wcpay.php
@@ -1,6 +1,9 @@
 <?php
 $postStr = file_get_contents ( 'php://input' );
+file_put_contents ( 't.txt', $postStr  );
+
 $result = simplexml_load_string ( $postStr );
+file_put_contents ( 't2.txt', serialize($result) );
 header('Location:/app/item/pay_c/'.$result->out_trade_no);
 
 
