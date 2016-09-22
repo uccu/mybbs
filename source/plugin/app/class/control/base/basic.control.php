8dbb36aabb40d0e800022b295217a4241d031a8e
uccu <DuanYaFei@jyliMac.local>
2016-09-22 10:22:58 +0800

    123

--- source/plugin/app/class/control/base/basic.control.php
+++ source/plugin/app/class/control/base/basic.control.php
@@ -39,14 +39,14 @@ class basic extends \control\ajax{
     
     function _get_aid(){
         $now = TIME_NOW;
-        $where['zz'] = "stime<$now AND etime>$now";
+        $where = "stime<$now AND etime>$now";
         $z = model('activity')->where($where)->find();
         if(!$z)$this->errorCode(419);
         return $z ? $z['aid'] : 0;
     }
     function _get_lastAid(){
         $now = TIME_NOW;
-        $where['zz'] = "stime<$now AND etime>$now";
+        $where = "stime<$now AND etime>$now";
         $z = model('activity')->where($where)->find();
         if($z)return $z['aid'];
         $where['etime'] = array('logic',$now,'<');
