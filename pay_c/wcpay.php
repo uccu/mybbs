203fa58a190fa8dbaa622eafb06b7075445399b9
uccu <DuanYaFei@jyliMac.local>
2016-09-23 10:20:43 +0800

    e

--- pay_c/wcpay.php
+++ pay_c/wcpay.php
@@ -1,5 +1,7 @@
 <?php
-
+$postStr = file_get_contents ( 'php://input' );
+preg_match('#(98\d{20})#',$postStr,$zk);
+file_put_contents ( 't2.txt', serialize($zk) );
 header('Location:/app/item/pay_c/'.$zk[1]);
 
 
--- source/plugin/tool/class/control/captcha.control.php
+++ source/plugin/tool/class/control/captcha.control.php
@@ -13,9 +13,10 @@ class captcha extends \control\ajax{
         for($i=0;$i<6;$i++){
             $s.=$a[rand(0,9)];
         }
-        $z = file_get_contents ("https://sapi.253.com/msg/HttpBatchSendSM?account=VIP-lssm-1&pswd=Key-147852&mobile={$phone}&needstatus=false&msg=您好，您的验证码是：{$s}");
+        $z['o'] = file_get_contents ("https://sapi.253.com/msg/HttpBatchSendSM?account=VIP-lssm-1&pswd=Key-147852&mobile={$phone}&needstatus=false&msg=您好，您的验证码是：{$s}");
         session_start();
         $_SESSION['captcha'] = $s;
+        $z['i'] = model('user')->where(array('phone'=>$phone))->find() ? 1 : 0;
         $this->success($z);
     }
     function _check_captcha(){
