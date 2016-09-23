6bad3fc28c56b762f5e6882712b0b4db979508c8
uccu <DuanYaFei@jyliMac.local>
2016-09-23 19:07:01 +0800

    w

--- source/plugin/app/class/control/item.control.php
+++ source/plugin/app/class/control/item.control.php
@@ -767,7 +767,7 @@ class item extends base\basic{
         // $sign = urlencode ( $sign );
         // echo $sign;
 
-        $r = openssl_verify($datag, $dataj['sign'], $res, "sha256WithRSAEncryption");
+        $r = openssl_verify($datag, $dataj['sign'], 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCnxj/9qwVfgoUh/y2W89L6BkRAFljhNhgPdyPuBV64bfQNN1PjbCzkIM6qRdKBoLPXmKKMiFYnkd6rAoprih3/PrQEB/VsW8OoM8fxn67UDYuyBTqA23MML9q1+ilIZwBC2AQ2UBVOrFXfFl75p6/B5KsiNG9zpgmLCUYuLkxpLQIDAQAB', "sha256WithRSAEncryption");
         var_dump($r);
     }
 
