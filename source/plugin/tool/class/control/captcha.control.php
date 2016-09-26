ad8b78fb129e474859e61b1f63d463d00d1694bb
uccu <DuanYaFei@jyliMac.local>
2016-09-26 14:45:21 +0800

    uu

--- source/plugin/tool/class/control/captcha.control.php
+++ source/plugin/tool/class/control/captcha.control.php
@@ -1,7 +1,7 @@
 <?php
 namespace plugin\tool\control;
 defined('IN_PLAY') || exit('Access Denied');
-class captcha extends \control\ajax{
+class captcha extends \plugin\app\control\base\basic{
     function _beginning(){
         $this->checkAJAX = 0;
         //var_dump($this->uid);die();
@@ -16,11 +16,17 @@ class captcha extends \control\ajax{
         $z['o'] = file_get_contents ("https://sapi.253.com/msg/HttpBatchSendSM?account=VIP-lssm-1&pswd=Key-147852&mobile={$phone}&needstatus=false&msg=您好，您的验证码是：{$s}");
         session_start();
         $_SESSION['captcha'] = $s;
+        $_SESSION['phone'] = $phone;
         $z['new'] = model('user')->where(array('phone'=>$phone))->find() ? 0 : 1;
         $this->success($z);
     }
     function _check_captcha(){
         session_start();
+        if($this->uid){
+            if($this->userInfo['phone']!=$_SESSION['phone'])$this->error(501,'手机号与预留的手机号不同');
+        }else{
+            if($_POST['phone']!=$_SESSION['phone'])$this->error(501,'手机号与预留的手机号不同');
+        }
         if($_SESSION['captcha'] !== post('captcha',-1)){
             $this->error(501,'验证码错误');
         }
