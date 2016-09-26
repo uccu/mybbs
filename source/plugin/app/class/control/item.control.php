3e4ecb60162f959c6b6562db3104f4bbeca4ae2c
uccu <DuanYaFei@jyliMac.local>
2016-09-26 15:32:00 +0800

    dqw

--- source/plugin/app/class/control/item.control.php
+++ source/plugin/app/class/control/item.control.php
@@ -234,7 +234,7 @@ class item extends base\basic{
     }
     function money($oids){
         $oids = explode(',',$oids);
-        $c = model('order')->where(array('oid'=>array('contain',$oids,'IN')))->get_field('SUM(`price`)');
+        $c = model('order')->where(array('oid'=>array('contain',$oids,'IN')))->get_field('SUM(`money`)');
         if(!$c)$c = 0;
         $data['count'] = $c; 
         $this->success($data);
