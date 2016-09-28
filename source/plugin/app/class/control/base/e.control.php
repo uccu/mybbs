8c8d94341a0b0c49c6cd32c50b1630dc658c86a9
uccu <DuanYaFei@jyliMac.local>
2016-09-28 11:44:16 +0800

    first

---
+++ source/plugin/app/class/control/base/e.control.php
@@ -0,0 +1,10 @@
+<?php
+namespace plugin\tool\control\base;
+defined('IN_PLAY') || exit('Access Denied');
+class e extends \control\ajax{
+    function _beginning(){
+        
+    }
+    
+}
+?>
---
+++ source/plugin/app/class/control/in.control.php
@@ -0,0 +1,12 @@
+<?php
+namespace plugin\tool\control;
+defined('IN_PLAY') || exit('Access Denied');
+class in extends base\e{
+    function _beginning(){
+        
+    }
+    function get_captcha(){
+        
+    }
+}
+?>
