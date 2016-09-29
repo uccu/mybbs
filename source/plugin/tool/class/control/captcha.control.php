593b766c72e5d4150efb1bc4ac597e8a7af492fc
uccu <DuanYaFei@jyliMac.local>
2016-09-29 13:35:13 +0800

    ec

--- source/plugin/tool/class/control/captcha.control.php
+++ source/plugin/tool/class/control/captcha.control.php
@@ -6,26 +6,26 @@ class captcha extends \plugin\app\control\base\basic{
         $this->checkAJAX = 0;
         //var_dump($this->uid);die();
     }
-    function get_captcha($phone){
-        $phone = post('phone',$phone);
-        if(!preg_match('#^1\d{10}$#',$phone))$this->error(500,'手机格式错误');
+    function get_captcha($usercode){
+        $usercode = post('usercode',$usercode);
+        if(!preg_match('#^1\d{10}$#',$usercode))$this->error(500,'手机格式错误');
         $s = '';$a = '1234567890';
         for($i=0;$i<6;$i++){
             $s.=$a[rand(0,9)];
         }
-        //$z['o'] = file_get_contents ("https://sapi.253.com/msg/HttpBatchSendSM?account=VIP-lssm-1&pswd=Key-147852&mobile={$phone}&needstatus=false&msg=您好，您的验证码是：{$s}");
+        //$z['o'] = file_get_contents ("https://sapi.253.com/msg/HttpBatchSendSM?account=VIP-lssm-1&pswd=Key-147852&mobile={$usercode}&needstatus=false&msg=您好，您的验证码是：{$s}");
         session_start();
         $_SESSION['captcha'] = $s;
-        $_SESSION['phone'] = $phone;
-        $z['new'] = model('user')->where(array('phone'=>$phone))->find() ? 0 : 1;
+        $_SESSION['usercode'] = $usercode;
+        $z['new'] = model('user')->where(array('usercode'=>$usercode))->find() ? 0 : 1;
         $this->success($z);
     }
     function _check_captcha(){
         session_start();
         if($this->uid>0){
-            if($this->userInfo['phone']!=$_SESSION['phone'])$this->error(501,'手机号与预留的手机号不同');
+            if($this->userInfo['usercode']!=$_SESSION['usercode'])$this->error(501,'手机号与预留的手机号不同');
         }else{
-            if($_POST['phone']!=$_SESSION['phone'])$this->error(502,'发送验证码手机号与注册手机号不同:');
+            if($_POST['usercode']!=$_SESSION['usercode'])$this->error(502,'发送验证码手机号与注册手机号不同:');
         }
         // if($_SESSION['captcha'] !== post('captcha',-1)){
         //     $this->error(501,'验证码错误');
