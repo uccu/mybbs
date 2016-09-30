81037c23a1250cf324b61886daf92d875800f44a
uccu <DuanYaFei@jyliMac.local>
2016-09-30 09:59:01 +0800

    ef

---
+++ source/plugin/app/class/control/equip.control.php
@@ -0,0 +1,20 @@
+<?php
+namespace plugin\app\control;
+defined('IN_PLAY') || exit('Access Denied');
+class equip extends base\e{
+    function _beginning(){
+        $this->_check_login();
+    }
+
+    function types($bid = 0){
+        $where['bid'] = post('bid',$bid,'%d');
+        $where['del'] = 1;
+        $f = model('equipment_list')->where($where)->limit(99)->order(array('orders'))->select();
+        if(!$f)$this->errorCode(427);
+        $this->success($f);
+    }
+
+
+
+}
+?>
