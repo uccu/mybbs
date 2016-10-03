8e6129a05877ce0ff360bbc9c52b7c4c17c0112f
uccu <418667631@qq.com>
2016-10-03 18:22:37 +0800

    ur

---
+++ source/plugin/tool/template/aa.php
@@ -0,0 +1,10 @@
+<!--{subtemplate header}-->
+<style>
+div{width:10px;height:10px;border-radius:50%;background:#ccc}
+div:hover{width:200px;height:200px;border-radius:0}
+
+</style>
+
+<div class="pa t"></div>
+
+<!--{subtemplate footer}-->
