0a6dc40be2d30d09f5cfa79beb79ddaa9f2166df
uccu <DuanYaFei@jyliMac.local>
2016-10-10 14:35:40 +0800

    123

--- source/plugin/app/class/control/inquiry.control.php
+++ source/plugin/app/class/control/inquiry.control.php
@@ -124,7 +124,7 @@ class inquiry extends base\e{
         else $where = '1=2';
         $page = post('page',1);
         $limit = post('limit',10);
-        $t['list'] = model('user')->sql()->field(array(
+        $t['list'] = model('user')->field(array(
             "uid","thumb","nickname","experience","label","type","is_login","answer","fans","follow","followed"
         ))->mapping('u')->add_table(array(
              'fans'=>array('_join'=>'LEFT JOIN','_on'=>'u.uid=f.uid AND f.fans_id='."'{$this->uid}'",'_mapping'=>'f','fans_id'=>'followed')
