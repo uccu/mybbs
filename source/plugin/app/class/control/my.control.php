2d15d4d3b9dd6bc0174cd8b1ed9f269e95e6ab77
uccu <DuanYaFei@jyliMac.local>
2016-10-11 14:29:38 +0800

    增加积分

--- source/plugin/app/class/control/my.control.php
+++ source/plugin/app/class/control/my.control.php
@@ -54,7 +54,7 @@ class my extends base\e{
     function my_info(){
         $u = $this->userInfo;
         unset($u['password']);
-        $t['info'] = model('user')->field(array('uid','nickname','type','label','thumb','sex'))->find($this->uid);
+        $t['info'] = model('user')->field(array('uid','nickname','type','label','thumb','sex','score'))->find($this->uid);
         
         $t['fans'] = model('fans')->where(array('uid'=>$this->uid))->get_field();
         $t['follow'] = model('fans')->where(array('follow'=>$this->uid))->get_field();
