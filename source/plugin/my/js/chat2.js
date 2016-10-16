a6f58b74ad3243130b1520eda49bf0dee8196be2
uccu <418667631@qq.com>
2016-10-16 23:26:15 +0800

    gt

--- source/plugin/my/js/chat2.js
+++ source/plugin/my/js/chat2.js
@@ -1,15 +1,35 @@
 !function(){
-    window.ws=null;var link = 'ws:127.0.0.1:7777/',uid,cid;
-
+    var link = 'ws:127.0.0.1:7777/',uid,cid,ws;
+    j('title').text('伪·直播~(*-ω-)ﾄﾞｷ');
     var mesCore = {
         
         user_login:function(obj){
             if(obj.status==200)uid = obj.uid;
             console.log('登陆成功~');
+            j('.login').addClass('channel').css({height:0,transform:'perspective(1000px) rotateY(90deg)','transform-origin': 'left'});
+            j('.overlay>.back').removeClass('active');
+            
+            setTimeout(function(){
+                j('.overlay>.back').addClass('active');                
+                j('.login>div').addClass('dn');
+                j('.login>div.channel').removeClass('dn');
+                j('.login').addClass('channel').css({height:200,transform:'scale(2) rotate(-5deg)','transform-origin': 'center'});
+            },650)
+
+
         },
         channel_login:function(obj){
             if(obj.status==200)cid = obj.cid;
             console.log('进入成功~');
+            j('.overlay>.back').addClass('dark');
+            j('.login').css({height:0,transform:'perspective(1000px) rotateY(90deg)','transform-origin': 'left'});
+            setTimeout(function(){
+                j('.login').addClass('dn');
+                j('input[type=file],video').removeClass('dn');
+            },1000)
+        },
+        channel_message:function(obj){
+            console.log(obj.nickname+':'+obj.message);
         },
         video_sync:function(obj){
             if(obj.uid==uid)return;
@@ -17,29 +37,28 @@
             var v = j('video')[0];
             v.currentTime = time;
             if(obj.paused && !v.paused){
-                j('video').unbind('pause');
+                // j('video').unbind('pause');
                 v.pause();
             }else if(!obj.paused && v.paused){
-                j('video').unbind('play pause');
+                //j('video').unbind('play pause');
                 v.play();
-                j('video').one('playing',function(){
-                    j('video').bind('play pause',function(){
-                        j('.sync').click();
-                    });
-                });
+                // j('video').one('playing',function(){
+                //     j('video').bind('play pause',function(){
+                //         sync()
+                //     });
+                // });
             }
         },
 
     };
 
-    j('.connect').bind('click',function(){
+    var connectF = function(c){
         if(ws)return;
-        j('.connect').unbind('click').addClass('disabled');
-
+        
         ws = new WebSocket(link);
-        ws.onopen = d=>{console.log(d)};
+        window.ws=ws;
+        ws.onopen = d=>{console.log('连接成功~');if(c && typeof c === 'function')c(ws)}
         ws.onmessage = d=>{
-            //console.log(d);
             try{
                 var v = JSON.parse(d.data);
                 if(typeof v !=='object')return;
@@ -50,61 +69,74 @@
         };
         ws.onerror = d=>{ws = undefined;console.log(d)};
         ws.onclose = d=>{ws = undefined;console.log(d)};
-    });
+        ws.sendMessageToChannel = function(x){
+            var obj = {type:'channel_message',message:x,cid:cid};
+            ws.send(JSON.stringify(obj));
+        }
+    };
+    j(function(){
+        connectF(function(){
+            j('.login').addClass('active');
+            j('.overlay>.back').addClass('active');
+        });
+    })
 
 
-    j('.login').bind('click',function(){
+
+    j('.login_a').bind('click',function(){
         if(!ws){alert('没有连接');return}
         if(!ws.readyState){alert('未准备就绪');return}
         j(this).unbind('click').addClass('disabled');
-        j('#password').attr('disabled','disabled');
-        var g = {type:'user_login',nickname:j('#nickname').val(),password:j('#password').val(),'uid':'x',avatar:'abc.jpg'};
+        j('#password,#uid,#nickname,#avatar').attr('disabled','disabled');
+        var g = {type:'user_login',nickname:j('#nickname').val(),password:j('#password').val(),'uid':j('#uid').val(),avatar:j('#avatar').val()};
         ws.send(JSON.stringify(g));
 
 
     });
 
-    j('.channel').bind('click',function(){
+    j('.channel_a').bind('click',function(){
         if(!uid){alert('未登录');return}
         j(this).unbind('click').addClass('disabled');
         j('#channel').attr('disabled','disabled');
-        var g = {type:'channel_login',cid:j('#channel').val()};
+        var g = {type:'channel_login',cid:j('#cid').val()};
         ws.send(JSON.stringify(g));
 
 
     });
 
     j('.info').bind('click',function(){
-        var g = {type:'user_info'};
+        var g = {type:'user_info',uid:uid};
         ws.send(JSON.stringify(g));
     });
     
 
-    j('.play').bind('click',function(){
-        var f = j('#file')[0].files[0];
+    var play = function(){
+        var f = j('input[type=file]')[0].files[0];
         if(!f)return;
         var u = URL.createObjectURL(f);
         j('video')[0].src = u;
         j('video').one('canplay',function(){j('video')[0].play();});
         
+    };
+
+    j('input[type=file]').bind('change',function(){
+        play()
     });
 
-    j('#file').bind('change',function(){
-        j('.play').click();
-    });
-
-    j('.sync').bind('click',function(){
+    var sync = function(){
         var v = j('video')[0];
-        var g = {type:'video_sync',cid:j('#channel').val(),time:v.currentTime,paused:v.paused?1:0};
+        var g = {type:'video_sync',cid:j('#cid').val(),time:v.currentTime,paused:v.paused?1:0};
         ws.send(JSON.stringify(g));
+    };
+
+
+    j('video').bind('play pause',function(e){
+        console.log(e.target.seeking)
+        if(e.type==='pause' && e.target.seeking)return;
+        sync()
     });
 
-
-    j('video').bind('play pause',function(){
-        j('.sync').click();
-    });
-
-    j('video')[0].controls = true;
+    //j('video')[0].controls = true;
 
 
 
--- source/plugin/my/template/chat2.php
+++ source/plugin/my/template/chat2.php
@@ -1,67 +1,155 @@
 <!--{subtemplate tool:header}-->
 <!--{eval addjs('classie',0,'tool')}-->
-<div class="jumbotron cd nos">
-    <div class="container">
-        
-    </div>
-</div>
-<div class="container">
 <style>
-video{width: 100%;}
-*{outline:0}
+.dn{display:none !important}
+body>.back{
+	width:100%;height:100%;z-index:-10;
+	background-image:url(/source/plugin/my/pic/pixiv_1039353.jpg);
+	background-size:cover;
+	background-position:center center;
+	filter:blur(10px);
+	-webkit-filter:blur(10px);
+}
+
+
+.overlay{
+	width:100%;height:100%;z-index:10;
+}
+.overlay>.back{
+	width:100%;height:100%;z-index:-1;top:0;left:0;
+	background-color:#000;
+	opacity:0.8;
+}
+.overlay>.back.active{opacity:0.5}
+.overlay>.back.dark{opacity:0.9}
+.overlay>.main{
+	display: table-cell;
+	vertical-align: middle;
+	text-align: center;
+	z-index:2;
+}
+.login{
+	width: 250px;
+	background-color: #fff;
+	padding: 25px;
+	padding-top: 50px;
+	box-shadow:3px 10px 30px #333;
+	opacity:0.95;
+	transform:scale(2) rotate(-5deg);
+	opacity:0;
+	filter:blur(1px);
+	height: 500px;
+    overflow: hidden;
+    padding-bottom: 0;
+	
+
+}
+.login.active{
+	filter:blur(0px);
+	transform:scale(1) rotate(0deg);opacity:0.95;
+}
+.login::before,.login::after{
+	transition: all 1s;-moz-transition: all 1s;-webkit-transition: all 1s;-o-transition: all 1s;
+	content:attr(before-content);position:absolute;
+	top:0;left:0;width:100%;height:50px;
+	background-color: rgba(94, 196, 80, 0.46);
+	text-align:left;font-size: 1.5em;
+    font-family: fantasy;padding: 10px 40px;opacity:1
+}
+
+.login::after{
+	content:attr(after-content);opacity:0
+}
+.login.channel::before{opacity:0}
+.login.channel::after{opacity:1}
+input{
+	height:57px;
+}
+.login_a,.channel_a{
+	background: transparent;
+    border: none;
+    font-size: 0.4em;
+    color: darkred;
+    font-weight: bold;
+    margin-right: 12px;padding:5px 15px;border-radius:3px;
+    outline: 0;
+}
+.login_a:active,.channel_a:active{
+	background-color:#ccc;color:#333
+}
+
+
 </style>
 
+<div class="back pf"></div>
 
+<div class="overlay pf dt">
+	<div class="back pa t-1"></div>
+	<div class="main pr">
+		<div class="login tc center-block pr t-1" before-content="Login" after-content="Channel">
+			<div class="tc">
+				<span class="input input--hoshi">
+					<input class="input__field input__field--hoshi" type="text" name="uid" id="uid">
+					<label class="input__label input__label--hoshi input__label--hoshi-color-3" for="uid">
+						<span class="input__label-content input__label-content--hoshi">UID</span>
+					</label>
+				</span>
+			</div>
+			<div class="tc">
+				<span class="input input--hoshi">
+					<input class="input__field input__field--hoshi" type="text" name="password" id="password">
+					<label class="input__label input__label--hoshi input__label--hoshi-color-3" for="password">
+						<span class="input__label-content input__label-content--hoshi">Password</span>
+					</label>
+				</span>
+			</div>
+			<div class="tc">
+				<span class="input input--hoshi">
+					<input class="input__field input__field--hoshi" type="text" name="nickname" id="nickname">
+					<label class="input__label input__label--hoshi input__label--hoshi-color-3" for="nickname">
+						<span class="input__label-content input__label-content--hoshi">Nickname</span>
+					</label>
+				</span>
+			</div>
+			<div class="tc">
+				<span class="input input--hoshi">
+					<input class="input__field input__field--hoshi" type="text" name="avatar" id="avatar">
+					<label class="input__label input__label--hoshi input__label--hoshi-color-3" for="avatar">
+						<span class="input__label-content input__label-content--hoshi">Avatar</span>
+					</label>
+				</span>
+			</div>
+			<div class="tr">
+				<button class="login_a t">登录</button>
+			</div>
 
-<div class="container">
-    <div class="row" style="padding:20px">
-        <button class="t btn btn-default btn-block connect">连接</button>
-    </div>
-    <div class="row" style="padding:20px">
-        <div class="col-md-6">
-            <span class="input input--hoshi">
-                <input class="input__field input__field--hoshi" type="password" id="password" name="password" style="height:57px;">
-                <label class="input__label input__label--hoshi input__label--hoshi-color-3" for="password">
-                    <span class="input__label-content input__label-content--hoshi">密码</span>
-                </label>
-            </span>
-        </div>
-        <div class="col-md-6">
-            <button class="t btn btn-default btn-block login" style="height:99px">登录</button>
-        </div>
-    </div>
-     <div class="row" style="padding:20px">
-        <div class="col-md-6">
-    
-            <span class="input input--hoshi">
-                <input class="input__field input__field--hoshi" type="text" id="channel" name="channel" style="height:57px">
-                <label class="input__label input__label--hoshi input__label--hoshi-color-3" for="channel">
-                    <span class="input__label-content input__label-content--hoshi">频道</span>
-                </label>
-            </span>
-         </div>
-        <div class="col-md-6">
-            <button class="t btn btn-default btn-block channel" style="height:99px">进入频道</button>
-        </div>
-    </div>
-    <span class="input input--hoshi">
-        <input class="input__field input__field--hoshi dn" type="file" id="file" name="file" style="height:57px;outline:0">
-        <label class="input__label input__label--hoshi input__label--hoshi-color-3" for="file">
-            <span class="input__label-content input__label-content--hoshi"></span>
-        </label>
-    </span>
+			<!--Channel-->
+			<div class="tc dn channel">
+				<span class="input input--hoshi">
+					<input class="input__field input__field--hoshi" type="text" name="cid" id="cid">
+					<label class="input__label input__label--hoshi input__label--hoshi-color-3" for="cid">
+						<span class="input__label-content input__label-content--hoshi">Cid</span>
+					</label>
+				</span>
+			</div>
+			<div class="tr dn channel">
+				<button class="channel_a t">进入频道</button>
+			</div>
+		</div>
+		<input type="file" class="dn" />
+		<video src="" class="dn" style="max-width:100%"></video>		
+	</div>
+
 </div>
 
 
 
-<button class="info">信息</button>
-
-<button class="play">播放</button>
-
-<button class="sync">同步</button>
 
 
-<video></video>
+
+
+
 <!--{eval addjs()}-->
-<!--{subtemplate _footer}-->
+
+<!--{subtemplate tool:footer}-->
 
