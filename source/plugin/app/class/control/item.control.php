1d1ac51471fa754f35d5384b565e3ad761ce5b73
uccu <DuanYaFei@jyliMac.local>
2016-10-17 13:49:32 +0800

    av

--- source/plugin/app/class/control/item.control.php
+++ source/plugin/app/class/control/item.control.php
@@ -19,8 +19,8 @@ class item extends base\basic{
         $where['aid'] = $aid;
         $q['list'] = model('goods')->mapping('g')->add_table(array(
             'activity_list'=>array('aid','_on'=>'tid','id'),
-            'goods_list_goods'=>array('_on'=>'tid','lid'=>'lgid')
-        ))->where($where)->order(array('id'=>'DESC'))->limit(999)->select();
+            'goods_list_goods'=>array('_on'=>'tid','lid'=>'lgid','location')
+        ))->where($where)->order(array('location'=>'DESC'))->limit(999)->select();
         if(!$q['list'])$this->errorCode(427);
         $this->success($q);
     }
