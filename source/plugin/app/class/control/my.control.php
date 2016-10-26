91f954d6ce1ab22d9e9480a0975e4092667b7c2e
uccu <mbmbn123@hotmail.com>
2016-10-26 16:19:30 +0800

    wq

--- source/plugin/app/class/control/my.control.php
+++ source/plugin/app/class/control/my.control.php
@@ -109,9 +109,9 @@ class my extends base\e{
         if(!$i)$this->errorCode(439);
         if(!$i['uid']!=$this->uid)$this->erorCode(411);
         $thank = post('thank',$thank);
-        model('inquiry_list')->where(array('adopt'=>1,'thank'=>$thank))->save($id);
+        $z['il'] = model('inquiry_list')->where(array('adopt'=>1,'thank'=>$thank))->save($id);
         model('inquiry')->where(array('finish'=>TIME_NOW))->save($r['bid']);
-        $this->success();
+        $this->success($z);
     }
 
     function del_inquiry($id){
