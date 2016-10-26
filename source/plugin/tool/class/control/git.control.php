476e92d446668b796ac6ba8b9b956a1bd27977cc
uccu <mbmbn123@hotmail.com>
2016-10-26 10:37:47 +0800

    Revert "test"
    This reverts commit 7be9fc41fe5c121c0c5f75224e82b6551c6fddb3.

--- source/plugin/tool/class/control/git.control.php
+++ source/plugin/tool/class/control/git.control.php
@@ -11,19 +11,19 @@ class git extends \control\ajax{
         $last_line = system('E:\phpStudy\WWW\lg\git_pull.bat',$retval);
         $array['last_line'] = $last_line;
         $array['retval'] = $retval;
-        //$this->success($array);
+        $this->success($array);
     }
     function ls(){
         $last_line = system('ls',$retval);
         $array['last_line'] = $last_line;
         $array['retval'] = $retval;
-        //$this->success($array);
+        $this->success($array);
     }
     function ping(){
         $last_line = system('ping baidu.com ',$retval);
         $array['last_line'] = $last_line;
         $array['retval'] = $retval;
-        //$this->success($array);
+        $this->success($array);
     }
 }
 ?>
