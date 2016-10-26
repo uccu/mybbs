71c1ff205c98bdb35c3ff85e8a2932c35b1c6769
uccu <mbmbn123@hotmail.com>
2016-10-26 17:32:58 +0800

    ef

--- source/plugin/app/class/control/equip.control.php
+++ source/plugin/app/class/control/equip.control.php
@@ -10,6 +10,21 @@ class equip extends base\e{
         $where['bid'] = post('bid',$bid,'%d');
         $where['del'] = 1;
         $f['list'] = model('equipment_list')->where($where)->limit(99)->order(array('orders'))->select();
+        foreach($f['list'] as &$v){
+            if($v['bid']){
+                $v['count'] = model('inquiry')->where(array('bid'=>$bid))->get_field();
+                $v['today_count'] = model('inquiry')->where(array('bid'=>$bid,'ctime'=>array('logic',$this->today,'>')))->get_field();
+            }else{
+                $v['count'] = model('inquiry')->mapping('i')->add_table(array(
+                    'equipment_list'=>array('_on'=>'i.bid=e.id','_mapping'=>'e','bid'=>'kid')
+                ))->where(array('kid'=>$bid))->get_field();
+                $v['today_count'] = model('inquiry')->mapping('i')->add_table(array(
+                    'equipment_list'=>array('_on'=>'i.bid=e.id','_mapping'=>'e','bid'=>'kid')
+                ))->where(array('kid'=>$bid,'ctime'=>array('logic',$this->today,'>')))->get_field();
+            }
+
+
+        }
         if(!$f['list'])$this->errorCode(427);
         $this->success($f);
     }
