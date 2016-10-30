23a29a87eb9562ecb3dbceac2ee831d4cdd99115
uccu <DuanYaFei@jyliMac.local>
2016-10-30 19:55:07 +0800

    dd

--- source/plugin/app/template/index.php
+++ source/plugin/app/template/index.php
@@ -3,15 +3,18 @@
     .T{min-width:1200px}
     .t1>img{width:100%}
     .nav{
-        width:100%;opacity:0.8;background-color:#000
+        width:100%;opacity:0.8;background-color:#000;padding:10px;top:0;left:0;
     }
+    .nav_e{max-width:1200px;margin:auto}
 </style>
 
-<div class="T t1">
+<div class="T t1 pr">
     <img src="/pic/background.png">
-    <div class="nav clear pa">
+    <div class="nav clear pa tc">
+        <div class="nav_e">
         <img src="/pic/logo.png" class="fl">
         <img src="/pic/phone.png" class="fr">
+        </div>
     </div>
 </div>
 
