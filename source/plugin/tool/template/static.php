a6cbd1f73fdc16ce674b77900c8d13412dd5c8e7
uccu <mbmbn123@hotmail.com>
2016-10-31 12:32:18 +0800

    qwd

--- source/plugin/tool/template/static.php
+++ source/plugin/tool/template/static.php
@@ -1,8 +1,11 @@
 <!--{subtemplate tool:header}-->
-
+<style>
+.container img{max-width:100%}
+.container p{font-size:14px}
+</style>
 <div class="container">
-<h2>{title}</h2>
-<p style="font-size:9px;color:#bbb">{time}</p>
+<h3>{title}</h3>
+<p style="font-size:12px;color:#bbb">{time}</p>
 {if $title}
 <hr style="color:#bbb">
 {/if}
