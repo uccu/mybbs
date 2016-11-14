cb90a5cdc7ee023f0107ffad4ad87ebcf4127041
uccu <DuanYaFei@jyliMac.local>
2016-11-14 13:45:39 +0800

    213

--- source/plugin/h5/template/expert/info.php
+++ source/plugin/h5/template/expert/info.php
@@ -1,5 +1,22 @@
 <!--{subtemplate header}-->
-123
+<div class="info-top">
+    <div class="avatar">
+        <img src="{info.thumb}">
+    </div>
+    <div class="name">
+        {info.nametrue}
+        <img src="/pic/h5/expert/zj.png">
+        {if $info['sex']==1}
+        <img src="/pic/h5/expert/male.png">
+        {elseif $info['sex']==2}
+        <img src="/pic/h5/expert/female.png">
+        {/if}
+    </div>
+    <div class="tag">
+        <p>{info.label}</p>
+        <p>从业经验：{info.experience}</p>
+    </div>
+</div>
 
 
 <!--{subtemplate tool:footer}-->
