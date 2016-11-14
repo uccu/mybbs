5366155c1ab6410db72305aa68c0434bac43ea34
uccu <DuanYaFei@jyliMac.local>
2016-11-14 15:02:44 +0800

    dqw

--- source/plugin/h5/template/expert/info.php
+++ source/plugin/h5/template/expert/info.php
@@ -18,23 +18,24 @@
         <p>从业经验：{info.experience}</p>
     </div>
 </div>
-<div class="info-mi row tc">
-
-    <div class="col-xs-3">
-        <div>{fans}</div>
-        <div class="text-muted">粉丝</div>
-    </div>
-    <div class="col-xs-3">
-        <div>{follow}</div>
-        <div class="text-muted">关注</div>
-    </div>
-    <div class="col-xs-3">
-        <div>{inquiry}</div>
-        <div class="text-muted">发布</div>
-    </div>
-    <div class="col-xs-3">
-        <div>{answer}</div>
-        <div class="text-muted">回答</div>
+<div class="info-mi tc">
+    <div class="row">
+        <div class="col-xs-3">
+            <div>{fans}</div>
+            <div class="text-muted">粉丝</div>
+        </div>
+        <div class="col-xs-3">
+            <div>{follow}</div>
+            <div class="text-muted">关注</div>
+        </div>
+        <div class="col-xs-3">
+            <div>{inquiry}</div>
+            <div class="text-muted">发布</div>
+        </div>
+        <div class="col-xs-3">
+            <div>{answer}</div>
+            <div class="text-muted">回答</div>
+        </div>
     </div>
 </div>
 
