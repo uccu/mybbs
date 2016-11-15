58847aa11bce4b7550a2e20eb78639b71fc9e5d2
uccu <DuanYaFei@jyliMac.local>
2016-11-15 15:08:19 +0800

    dq

--- source/plugin/h5/template/inquiry/type.php
+++ source/plugin/h5/template/inquiry/type.php
@@ -11,23 +11,25 @@
 <div class="list container-fluid">
     <!--{loop $list $v}-->
     <!--{eval foreach($v as $k=>$v2)$$k=$v2}-->
-    <div class="row">
-        <div class="col-xs-2 avatar">
-            <a href="/h5/inquiry/type/{id}"><img src="{img}"></a>
-        </div>
-        <div class="col-xs-7">
-            <div class="name">
-                <a href="/h5/inquiry/type/{id}">{name}</a>
+    <a href="/h5/inquiry/type/{id}">
+        <div class="row">
+            <div class="col-xs-2 avatar">
+                <img src="{img}">
             </div>
-            <div class="tag">
-                <p>问题总数 <font> {count} </font> 条</p>
+            <div class="col-xs-7">
+                <div class="name">
+                    {name}
+                </div>
+                <div class="tag">
+                    <p>问题总数 <font> {count} </font> 条</p>
+                </div>
+            </div>
+            <div class="col-xs-3 right tr">
+                {if $today_count}<span>{today_count}</span>{/if}
+                <img src="/pic/h5/inquiry/arrow@2x.png">
             </div>
         </div>
-        <div class="col-xs-3 right tr">
-            {if $today_count}<span>{today_count}</span>{/if}
-            <img src="/pic/h5/inquiry/arrow@2x.png">
-        </div>
-    </div>
+    </a>
     <!--{/loop}-->
 </div>
 
