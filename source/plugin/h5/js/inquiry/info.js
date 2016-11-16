66dbff11eca11318fcbab6462ac017b393738ab8
uccu <DuanYaFei@jyliMac.local>
2016-11-16 11:40:01 +0800

    dqdw

--- source/plugin/h5/js/inquiry/info.js
+++ source/plugin/h5/js/inquiry/info.js
@@ -4,7 +4,7 @@
         j('.large-pic .pic').eq(0).addClass('active');
         var l = j('.large-pic .pic').length;
 
-        var next = function(){
+        window.next = function(){
             if(!j('.large-pic .pic.active').length)return;
             var n = j('.large-pic .pic.active').index();
             j('.large-pic .pic').removeClass('active');
@@ -18,6 +18,22 @@
                     j(this).css('left',(j(this).index()-n-1)*100+'%')
                 })
                 j('.large-pic .pic').eq(n+1).addClass('active')
+            }
+        }
+        window.last = function(){
+            if(!j('.large-pic .pic.active').length)return;
+            var n = j('.large-pic .pic.active').index();
+            j('.large-pic .pic').removeClass('active');
+            if(!n){
+                j('.large-pic .pic').each(function(e){
+                    j(this).css('left',j(this).index()-l+1+'00%');
+                })
+                j('.large-pic .pic').eq(l-1).addClass('active')
+            }else{
+                j('.large-pic .pic').each(function(){
+                    j(this).css('left',(j(this).index()-n+1)*100+'%')
+                })
+                j('.large-pic .pic').eq(n-1).addClass('active')
             }
 
         }
