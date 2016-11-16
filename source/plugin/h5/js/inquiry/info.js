53da8e07ae6dead73c5273b735a1dbe110b9362d
uccu <DuanYaFei@jyliMac.local>
2016-11-16 11:21:13 +0800

    dqw

--- source/plugin/h5/js/inquiry/info.js
+++ source/plugin/h5/js/inquiry/info.js
@@ -1,11 +1,10 @@
 ~function(){
 
     j(function(){
-        j('.large-pic .pic').eq(1).css('left','100%')
-        j('.large-pic .pic').eq(2).css('left','200%')
+
 
         var next = function(){
-
+            
 
         }
     })
