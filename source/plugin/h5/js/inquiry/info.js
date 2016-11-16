e49ee63de6222c0f824e48ccef52d265c9ea819f
uccu <DuanYaFei@jyliMac.local>
2016-11-16 11:31:05 +0800

    dq

--- source/plugin/h5/js/inquiry/info.js
+++ source/plugin/h5/js/inquiry/info.js
@@ -1,10 +1,22 @@
 ~function(){
 
     j(function(){
-
+        j('.large-pic .pic').eq(0).addClass('active');
+        var l = j('.large-pic .pic').length;
 
         var next = function(){
-            
+            if(!j('.large-pic .pic.active').length)return;
+            var n = j('.large-pic .pic.active').index();
+            j('.large-pic .pic').removeClass('active');
+            if(l==n+1){
+                j('.large-pic .pic').each(function(e){
+                    j(e).css('left',j(e).index()+'00%');
+                })
+            }else{
+                j('.large-pic .pic').each(function(e){
+                    j(e).css('left',(j(e).index()-n-1)*100+'%');
+                })
+            }
 
         }
     })
