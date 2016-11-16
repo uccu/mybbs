5157fe82db1c21996c30cd4b8ec4eb6a3ef1b377
uccu <DuanYaFei@jyliMac.local>
2016-11-16 11:02:33 +0800

    dqw

--- source/plugin/h5/template/inquiry/info.php
+++ source/plugin/h5/template/inquiry/info.php
@@ -5,7 +5,7 @@
 <div class="banner ofh pr">
     <div class="large-pic pr">
         <!--{loop $img $v}-->
-        <div class="pic" style="background-image:url({v})"></div>
+        <div class="pic pa" style="background-image:url({v})"></div>
         <!--{/loop}-->
     </div>
     <div class="small-pic">
