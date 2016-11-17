7883a6c40d28ef1182e4386603183e959733ed4c
uccu <DuanYaFei@jyliMac.local>
2016-11-17 13:59:04 +0800

    qff

--- source/plugin/h5/template/contact.php
+++ source/plugin/h5/template/contact.php
@@ -17,27 +17,35 @@ p{color: #999;
     <h4>联系方式</h4>
 
     <div class="row">
-        <div class="col-xs-2">
+        <div class="col-xs-2 tr">
             <img src="/pic/h5/contact/phone@2x.png" alt="">
         </div>
-        <div class="col-xs-10" style="background:url(/pic/h5/contact/b.png)">
+        <div class="col-xs-10" style="background:url(/pic/h5/contact/b.png);background-size: 100% 100%;padding: 20px 40px;width: 280px;">
             电话：<font style="color:orange;font-size:16px">400-666-666</font> （免费）
         </div>
     </div>
     <div class="row">
-        <div class="col-xs-2">
+        <div class="col-xs-2 tr">
             <img src="/pic/h5/contact/email@2x.png" alt="">
         </div>
-        <div class="col-xs-10" style="background:url(/pic/h5/contact/b.png)">
+        <div class="col-xs-10" style="background:url(/pic/h5/contact/b.png);background-size: 100% 100%;padding: 20px 40px;width: 255px;">
             邮箱：yunweiws@126.com
         </div>
     </div>
-    <p style="color:#015caf;margin-top:25px">欢迎前来我司参观与考察</p>
     <div class="row">
-        <div class="col-xs-2">
+        <div class="col-xs-2 tr">
+
+        </div>
+        <div class="col-xs-10">
+             <p style="color:#015caf;margin-top:25px">欢迎前来我司参观与考察</p>
+        </div>
+    </div>
+   
+    <div class="row">
+        <div class="col-xs-2 tr">
             <img src="/pic/h5/contact/addr@2x.png" alt="">
         </div>
-        <div class="col-xs-10" style="background:url(/pic/h5/contact/b.png)">
+        <div class="col-xs-10" style="background:url(/pic/h5/contact/b.png);background-size: 100% 100%;padding: 20px 40px;width: 340px;">
             地址：上海市上宁区延安西路777号601室
         </div>
     </div>
