62f18c8ea5c78a4e22e0018c3c092c15a00bde9e
uccu <DuanYaFei@jyliMac.local>
2016-11-17 11:10:46 +0800

    fqv

---
+++ source/plugin/h5/template/bussiness.php
@@ -0,0 +1,52 @@
+<!--{subtemplate header}-->
+
+<div class="container">
+    <h4>合作前言</h4>
+    <p>
+        运维卫士APP隶属于上海擎测机电工程技术有限公司（以下简称“擎測机电”）版权所有，任何第三方或个人不得未经允许以任何名义进行推广、使用和宣传。
+    </p>
+    <p>
+        请遵从并详细阅读<font>《商务合作协议细则》</font>的相关条款
+    </p>
+</div>
+
+
+<div class="container">
+    <h4>合作联系方式</h4>
+    <p>
+        全国统一合作电话：400-666-666 （免费）
+    </p>
+    <p>
+        邮箱：Coop@qingce.com
+    </p>
+    <p>
+        来信请寄：Coop@qingce.com
+    </p>
+    <p>
+        欢迎前来我司当面进行合作商洽！
+    </p>
+    <p>
+        地址：上海市上宁区延安西路777号601室
+    </p>
+</div>
+
+<div class="container">
+    <h4>合作伙伴<footer>（排名不分先后）</footer></h4>
+    <div class="row">
+        <div class="col-xs-3">
+            <img src="/pic/h5/bussiness/1@2x.png" class="img-responsive">
+        </div>
+        <div class="col-xs-3">
+            <img src="/pic/h5/bussiness/2@2x.png" class="img-responsive">
+        </div>
+        <div class="col-xs-3">
+            <img src="/pic/h5/bussiness/3@2x.png" class="img-responsive">
+        </div>
+        <div class="col-xs-3">
+            <img src="/pic/h5/bussiness/4@2x.png" class="img-responsive">
+        </div>
+    </div>
+</div>
+
+
+<!--{subtemplate tool:footer}-->
