92339896b7ab1103dd99fab6ffeff601b52dcdda
uccu <DuanYaFei@jyliMac.local>
2016-11-17 11:41:51 +0800

    fqf

---
+++ source/plugin/h5/template/contact.php
@@ -0,0 +1,47 @@
+<!--{subtemplate header}-->
+<style>
+h4{font-size:20px;margin-bottom:20px}
+p{color: #999;
+    line-height: 24px;}
+</style>
+<div class="container" style="margin-top: 20px;">
+    <h4>关于运维卫士</h4>
+    <p style="margin-bottom: 0">
+        运维卫士APP隶属于上海擎测机电工程技术有限公司版权所有，APP主要为电厂及机电相关的用户提供综合性的在线咨询与问诊，以及针对性的课程与考试的专业服务平台，为用户提供最全面的设备问题解决方案。
+    </p>
+
+</div>
+
+
+<div class="container" style="margin-top: 40px;">
+    <h4>联系方式</h4>
+
+    <div class="row">
+    
+        <div class="col-xs-2">
+        
+        </div>
+        <div class="col-xs-10"></div>
+    
+    </div>
+    <p>
+        全国统一合作电话：<font style="color:orange;font-size:16px">400-666-666</font> （免费）
+    </p>
+    <p>
+        邮箱：Coop@qingce.com
+    </p>
+    <p>
+        来信请寄：Coop@qingce.com
+    </p>
+    <p style="color:#015caf;margin-top:25px">
+        欢迎前来我司当面进行合作商洽！
+    </p>
+    <p>
+        地址：上海市上宁区延安西路777号601室
+    </p>
+</div>
+
+
+
+
+<!--{subtemplate tool:footer}-->
