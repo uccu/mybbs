b6b260754b5a7e0d5504880f5f4fdce3c49354a1
uccu <DuanYaFei@jyliMac.local>
2016-11-17 17:40:47 +0800

    fc

--- source/plugin/app/class/control/user.control.php
+++ source/plugin/app/class/control/user.control.php
@@ -145,10 +145,10 @@ class user extends base\e{
         if($this->userInfo['qust'])model('user')->data(array('qust'=>array('add',-1)))->save($this->uid);
 
         $data['qust'] = $this->userInfo['qust'];
-        $pay = model('expert')->where(array('uid'=>$this->uid,'id'=>$uid))->find();
+        $pay = model('expert_paid')->where(array('uid'=>$this->uid,'id'=>$uid))->find();
 
         $data['paid'] = $pay && $pay['ctime']>TIME_NOW-24*3600 ? '1' : '0';
-        
+
         // $o = $this->_getCloudToken($uid);
 
         // if(!$data)$this->errorCode(429);
