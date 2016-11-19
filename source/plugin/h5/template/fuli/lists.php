ccf625d55fb8f2b7d519db111f356a14f0084340
uccu <DuanYaFei@jyliMac.local>
2016-11-19 13:07:23 +0800

    dqv

--- source/plugin/h5/template/fuli/lists.php
+++ source/plugin/h5/template/fuli/lists.php
@@ -8,13 +8,17 @@
     <!--{eval foreach($v as $k=>$v2)$$k=$v2}-->
     <div class="row" style="padding: 15px 0;border-bottom: 2px solid #f1f1f1;background:#fff">
         <div class="col-xs-3 avatar">
-            <img src="{thumb}" style="margin-top: 20px;">
+            {if $type==1}
+            <img src="/pic/h5/fuli/h@2x.png" style="margin-top: 20px;height:52px">
+            {else}
+            <img src="/pic/h5/fuli/f@2x.png" style="margin-top: 20px;height:52px">
+            {/if}
         </div>
         <div class="col-xs-9">
             <h4 class="name">{title}</h4>
             <div class="tag">
                 <p style="color:#a1a1a1;">{content}　</p>
-                <a href="/h5/fili/info/{id}" style="background: #ffedbf;border: 0;border-radius: 30px;padding: 2px 17px;color: #ff9c00;">查看详情</a>
+                <a href="/h5/fuli/info/{id}" style="background: #ffedbf;border: 0;border-radius: 30px;padding: 2px 17px;color: #ff9c00;">查看详情</a>
             </div>
         </div>
     </div>
