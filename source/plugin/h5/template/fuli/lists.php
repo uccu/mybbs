23211f0945998b94ebe6539f53eab94d5e79e3d7
uccu <DuanYaFei@jyliMac.local>
2016-11-19 13:09:44 +0800

    dqwv

--- source/plugin/h5/template/fuli/lists.php
+++ source/plugin/h5/template/fuli/lists.php
@@ -7,14 +7,14 @@
     <!--{loop $list $v}-->
     <!--{eval foreach($v as $k=>$v2)$$k=$v2}-->
     <div class="row" style="padding: 15px 0;border-bottom: 2px solid #f1f1f1;background:#fff">
-        <div class="col-xs-3 avatar">
+        <div class="col-xs-2 avatar">
             {if $type==1}
             <img src="/pic/h5/fuli/h@2x.png" style="margin-top: 20px;height:52px">
             {else}
             <img src="/pic/h5/fuli/f@2x.png" style="margin-top: 20px;height:52px">
             {/if}
         </div>
-        <div class="col-xs-9">
+        <div class="col-xs-10">
             <h4 class="name">{title}</h4>
             <div class="tag">
                 <p style="color:#a1a1a1;">{content}　</p>
