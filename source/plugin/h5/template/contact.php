d7a54b3c2078344c498292df1d770a11836cdcd9
uccu <DuanYaFei@jyliMac.local>
2016-11-19 13:32:09 +0800

    qwfqc

--- source/plugin/h5/template/contact.php
+++ source/plugin/h5/template/contact.php
@@ -20,16 +20,16 @@ p{color: #999;
         <div class="col-xs-2 tr">
             <img src="/pic/h5/contact/phone@2x.png" alt="" style="height: 49px;">
         </div>
-        <div class="col-xs-10" style="background:url(/pic/h5/contact/b.png);background-size: 100% 100%;padding: 10px 40px;width: 280px;">
-            电话：<font style="color:orange;font-size:16px">400-666-666</font> （免费）
+        <div class="col-xs-10" style="background:url(/pic/h5/contact/b.png);background-size: 100% 100%;padding: 10px 0 10px 30px;width: 200px;">
+            <font style="color:orange;font-size:16px">400-666-666</font> （免费）
         </div>
     </div>
     <div class="row">
         <div class="col-xs-2 tr">
             <img src="/pic/h5/contact/email@2x.png" alt="" style="height: 49px;">
         </div>
-        <div class="col-xs-10" style="background:url(/pic/h5/contact/b.png);background-size: 100% 100%;padding: 10px 40px;width: 255px;;color: #999;">
-            邮箱：yunweiws@126.com
+        <div class="col-xs-10" style="background:url(/pic/h5/contact/b.png);background-size: 100% 100%;padding: 10px 0 10px 30px;width: 180px;color: #999;">
+            yunweiws@126.com
         </div>
     </div>
     <div class="row">
@@ -45,8 +45,8 @@ p{color: #999;
         <div class="col-xs-2 tr">
             <img src="/pic/h5/contact/addr@2x.png" alt="" style="height: 49px;">
         </div>
-        <div class="col-xs-10" style="background:url(/pic/h5/contact/b.png);background-size: 100% 100%;padding: 10px 40px;width: 340px;">
-            地址：上海市上宁区延安西路777号601室
+        <div class="col-xs-10" style="background:url(/pic/h5/contact/b.png);background-size: 100% 100%;padding: 10px 0 10px 30px;width: 240px;font-size: 10px;">
+            上海市长宁区延安西路777号601室
         </div>
     </div>
     
