23ab7831cdda8965b37570dff354056c6ea7ada8
company <mbmbn123@hotmail.com>
2017-08-31 11:11:02 +0800

    dawdaw

--- source/plugin/app/class/control/xj.control.php
+++ source/plugin/app/class/control/xj.control.php
@@ -164,7 +164,7 @@ class xj extends base\e{
         $this->_check_login();
 
         $area_id = post('area_id',$area_id);
-        if($_POST['data'])$_POST['data'] = str_ireplace(array('&lt;','&gt;','&quot;','&#39;',array('<','>','"',"'")),$_POST['data']);
+        if($_POST['data'])$_POST['data'] = str_ireplace(array('&lt;','&gt;','&quot;','&#39;'),array('<','>','"',"'"),$_POST['data']);
         $data = $_POST['data']?$_POST['data']:$data;
         // $data = '[{"input_type":"0","value":"30","type":"3","bid":"3","parameters_id":"1"}]';
 
