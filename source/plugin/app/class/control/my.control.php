64db39eb3ea6960a4dedc2e25b3c5fbb216823a8
company <mbmbn123@hotmail.com>
2017-09-04 14:27:27 +0800

    daw

--- source/plugin/app/class/control/my.control.php
+++ source/plugin/app/class/control/my.control.php
@@ -884,55 +884,7 @@ class my extends base\e{
 
 
     public function getFirstChar($s){  
-       $s0 = mb_substr($s,0,1);
-        $s = iconv('UTF-8','gb2312', $s0);
-        if (ord($s0)>128) {
-            $asc=ord($s{0})*256+ord($s{1})-65536;  
-            if($asc>=-20319 and $asc<=-20284)return "A";  
-            if($asc>=-20283 and $asc<=-19776)return "B";  
-            if($asc>=-19775 and $asc<=-19219)return "C";  
-            if($asc>=-19218 and $asc<=-18711)return "D";  
-            if($asc>=-18710 and $asc<=-18527)return "E";  
-            if($asc>=-18526 and $asc<=-18240)return "F";  
-            if($asc>=-18239 and $asc<=-17923)return "G";  
-            if($asc>=-17922 and $asc<=-17418)return "I";               
-            if($asc>=-17417 and $asc<=-16475)return "J";               
-            if($asc>=-16474 and $asc<=-16213)return "K";               
-            if($asc>=-16212 and $asc<=-15641)return "L";               
-            if($asc>=-15640 and $asc<=-15166)return "M";               
-            if($asc>=-15165 and $asc<=-14923)return "N";               
-            if($asc>=-14922 and $asc<=-14915)return "O";               
-            if($asc>=-14914 and $asc<=-14631)return "P";               
-            if($asc>=-14630 and $asc<=-14150)return "Q";               
-            if($asc>=-14149 and $asc<=-14091)return "R";               
-            if($asc>=-14090 and $asc<=-13319)return "S";               
-            if($asc>=-13318 and $asc<=-12839)return "T";               
-            if($asc>=-12838 and $asc<=-12557)return "W";               
-            if($asc>=-12556 and $asc<=-11848)return "X";               
-            if($asc>=-11847 and $asc<=-11056)return "Y";               
-            if($asc>=-11055 and $asc<=-10247)return "Z";   
-        }else if(ord($s)>=48 and ord($s)<=57){
-            switch(iconv_substr($s,0,1,'utf-8')){  
-                case 1:return "Y";  
-                case 2:return "E";  
-                case 3:return "S";  
-                case 4:return "S";  
-                case 5:return "W";  
-                case 6:return "L";  
-                case 7:return "Q";  
-                case 8:return "B";  
-                case 9:return "J";  
-                case 0:return "L";  
-            }                 
-        }else if(ord($s)>=65 and ord($s)<=90){
-            return substr($s,0,1);  
-        }else if(ord($s)>=97 and ord($s)<=122){
-            return strtoupper(substr($s,0,1));  
-        }  
-        else  
-        {  
-            return iconv_substr($s0,0,1,'utf-8');  
-        }  
+       return Hanzi\Hanzi::pinyin($s)['pinyin'];
     }
 
     public function orderByName($userName){  
@@ -940,7 +892,8 @@ class my extends base\e{
         sort($userName);
         $charArray = [];
         foreach($userName as $name){  
-            $char = $this->getFirstChar($name['nickname']);  
+            $char = substr( $this->getFirstChar($name['nickname']),0,1);
+            // var_dump($char);
             $nameArray = array();
             if(count($charArray[$char])!=0){  
                 $nameArray = $charArray[$char];  
@@ -948,14 +901,14 @@ class my extends base\e{
             array_push($nameArray,$name);   
             $charArray[$char] = $nameArray;  
         }  
-
+        
         return $charArray;  
     }
 
 
     function my_follow_sort($type){
         $type = post('type',$type,'%d');
-        $where['fans_id'] = $this->uid;
+        $where['fans_id'] = $this->uid = 502;
         $type && $where['did'] = $type;
         model('fans')->mapping('f');
         $t['follow'] = model('fans')->add_table(array(
